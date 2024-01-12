e2fa4dcfbf2d4243afc58e83af5e6b9d578b9e4f
Gladkov Sergey <melcor2006@mail.ru>
2024-01-12 20:40:50 +0300

    Add def title_string

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def upper_string(text):
+    """Делает все буквы заглавными."""
     return text.upper()
+
+
+def title_string(text):
+    """Делает первые буквы заглавными."""
+    return text.title()
